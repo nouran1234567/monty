f40be402f039407a700a53c72eb9a173677e2243
nouran1234567 <nouran.a.mohamed@hotmail.com>
2023-09-21 16:45:25 -0700

    update

--- mainy.c
+++ mainy.c
@@ -1,38 +1,34 @@
 #include "monty.h"
 
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t nlines = 0;
-	char *lines[2] = {NULL, NULL};
+	
+	char *liny = NULL;
+	ssize_t ready;
+	size_t leny = 0;
+	FILE *fily;
+	unsigned int line_nmbr = 0;
+	stack_t *stacky = NULL;
 
-	fd = check_input(argc, argv);
-	start_vglo(fd);
-	nlines = getline(&vglo.buffer, &size, fd);
-	while (nlines != -1)
-	{
-		lines[0] = _strtoky(vglo.buffer, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
-		{
-			f = get_opcodes(lines[0]);
-			if (!f)
-			{
-				dprintf(2, "L%u: ", vglo.cont);
-				dprintf(2, "unknown instruction %s\n", lines[0]);
-				free_vglo();
-				exit(EXIT_FAILURE);
-			}
-			vglo.arg = _strtoky(NULL, " \t\n");
-			f(&vglo.head, vglo.cont);
-		}
-		nlines = getline(&vglo.buffer, &size, fd);
-		vglo.cont++;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s monty_file\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
-	free_vglo();
+	fily = fopen(argv[1], "r");
+	if (fily == NULL) {
+		fprintf(stderr, "Error opening file: %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-	return (0);
+	while ((ready = getline(&liny, &leny, fily)) != -1) 
+	{
+		line_nmbr++;
+
+		parse_line(liny, line_nmbr, &stacky);
+	}
+
+	free_stacky(&stacky);
+	
+	return 0;
 }
